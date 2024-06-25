7c387ca150d3ef9ca26c77d8b0c055ac26816ec9
fukumasi <fukumasi@gmail.com>
2024-06-26 03:45:28 +0900

    Reinstall mongoose and fix configuration

--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
-
 const app = express();
 
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = "mongodb+srv://fukumasi:JU9PiGhLaRTdDlYs@cluster0.l1ibnnc.mongodb.net/yourDB?retryWrites=true&w=majority";
 
-mongoose.connect("mongodb+srv://fukumasi:JU9PiGhLaRTdDlYs@cluster0.l1ibnnc.mongodb.net/my-video-finder", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log("MongoDB connected..."))
-  .catch(err => console.log(err));
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+// MongoDB接続
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("MongoDB connected...");
+}).catch(err => {
+    console.error("MongoDB connection error:", err);
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}/`));
+// ミドルウェア
+app.use(express.json());
+
+// サンプルルート
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// サーバー起動
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
