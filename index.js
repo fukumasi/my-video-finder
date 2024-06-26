7e0bab20a131f89d9f1ddc6df100a636fc2c5cbc
fukumasi <fukumasi@gmail.com>
2024-06-27 01:41:16 +0900

    Remove deprecated Mongoose options

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ const PORT = process.env.PORT || 3000;
 const MONGO_URI = "mongodb+srv://fukumasi:JU9PiGhLaRTdDlYs@cluster0.l1ibnnc.mongodb.net/yourDB?retryWrites=true&w=majority";
 
 // MongoDB接続
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(MONGO_URI).then(() => {
     console.log("MongoDB connected...");
 }).catch(err => {
     console.error("MongoDB connection error:", err);
